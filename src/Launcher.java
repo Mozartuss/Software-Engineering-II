e252258d93cd3874f2e80a5e0d51ba0772771367
Mozartuss <32893711+Mozartuss@users.noreply.github.com>
2018-06-09 21:03:47 +0200

    7

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,12 +1,15 @@
 public class Launcher {
-    public static void main(String[] args) {
-        BookSearch bs = new BookSearch.BookSearchBuilder("software engineering").build();
+
+  public static void main(String[] args) {
+
+    BookSearch bs = new BookSearch.BookSearchBuilder("software engineering")
+        .publicationYear(2017)
+        .build();
+
+    System.out.println(bs.getKeyword() + " " + bs.getAuthor() + " " + bs.getPublicationYear());
 
 
-        System.out.println(bs.getKeyword() + " " + bs.getAuthor() + " " + bs.getPublicationYear());
-
-
-    }
+  }
 }
 
 
