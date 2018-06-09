b8c1e8746853eedac03a90a72a2d1491b8487738
Mozartuss <32893711+Mozartuss@users.noreply.github.com>
2018-06-10 00:35:01 +0200

    Aufgabe 7

---
+++ src/Aufgabe7/BookSearch.java
@@ -0,0 +1,75 @@
+package Aufgabe7;
+
+public class BookSearch {
+  private final String keyword; //required
+  private final String author; //optional default: null
+  private final int publicationYear; //optional default: Integer.MAX_VALUE
+
+  /**
+   * The method can't be called from outside this class.
+   *
+   * @param builder BookSearchBuilder
+   */
+
+  private BookSearch(BookSearchBuilder builder) {
+    this.keyword = builder.keyword;
+    this.author = builder.author;
+    this.publicationYear = builder.publicationYear;
+  }
+
+  /**
+   * Every attribute get his getterMethod.
+   *
+   * @return the attribute
+   */
+
+  public String getKeyword() {
+    return keyword;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public int getPublicationYear() {
+    return publicationYear;
+  }
+
+  /**
+   * The builder constructor only receives the required attributes
+   * and this attributes are the only ones that are defined “final” on the builder.
+   * Here you can set the default values.
+   */
+
+  public static class BookSearchBuilder {
+    private final String keyword;
+    private String author = "null";
+    private int publicationYear = Integer.MAX_VALUE;
+
+    public BookSearchBuilder(String keyword) {
+      this.keyword = keyword;
+    }
+
+    public BookSearchBuilder author(String author) {
+      this.author = author;
+      return this;
+    }
+
+    public BookSearchBuilder publicationYear(int publicationYear) {
+      this.publicationYear = publicationYear;
+      return this;
+    }
+
+    /**
+     * By returning the builder each time, we can create a fluent interface.
+     *
+     * @return builder
+     */
+
+    public BookSearch build() {
+      return new BookSearch(this);
+    }
+  }
+}
+
+
--- src/Aufgabe7/Launcher.java
+++ src/Aufgabe7/Launcher.java
@@ -1,13 +1,26 @@
+package Aufgabe7;
+
 public class Launcher {
+
+  /**
+   * Now we can call the BookSearch over a fluent interface so it is more readable.
+   *
+   * @param args -args
+   */
 
   public static void main(String[] args) {
 
     BookSearch bs = new BookSearch.BookSearchBuilder("software engineering")
+
+        //here you can call the attributes in any order you want.
+
         .publicationYear(2017)
+
+        //but .build() is always the end of your call.
+
         .build();
 
     System.out.println(bs.getKeyword() + " " + bs.getAuthor() + " " + bs.getPublicationYear());
-
 
   }
 }
